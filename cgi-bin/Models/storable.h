90226255dcdc1e189c311dfd52ae3feeb6beb663
Linsheng Ding <lding43@uwo.ca>
2018-11-24 16:54:25 -0500

    Changed up models

--- cgi-bin/Models/storable.h
+++ cgi-bin/Models/storable.h
@@ -5,27 +5,24 @@
 
 class Storable {
     public:
-        Storable(const std::string& name, const std::string &address) : _name(name), _address(address) {
+        Storable(const std::string& name, const std::string &address) : _name(name), _address(address) {};
 
-        }
-        virtual ~Storable();
+        virtual ~Storable() {};
         
         virtual std::string to_string() {
             std::string res = "File: " + this->_name + " Address: " + this->_address;
             return res;
-        }
+        };
         
-        virtual void append(Storable* newStorable) {
+        virtual void append(Storable* newStorable) {};
 
-        }
-
-        virtual const std::string getName() const {
+        virtual const std::string& getName() const {
             return this->_name;
-        }
+        };
 
-        virtual const std::string getAddress() const {
+        virtual const std::string& getAddress() const {
             return this->_address;
-        }
+        };
 
     private:
         std::string _name;
