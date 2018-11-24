bf672b0ff93f5c7c02f7932aee885f795cf9b709
Michael Ding <m.ding5692@gmail.com>
2018-11-24 16:11:13 -0500

    Updates fileSever + folder

--- cgi-bin/Models/folder.cpp
+++ cgi-bin/Models/folder.cpp
@@ -9,10 +9,10 @@ Folder::Folder(const string& name, const string& address) : Storable(name, addre
 
 // Adds storable to folder
 void Folder::append(Storable* newStorable) {
-	this->storedContent.push_back(newStorable);
+	this->_storedContent.push_back(newStorable);
 }
 
 // Returns files as vector of files
-vector<File> Folder::getStoredFiles() {
+vector<Storable*> Folder::getStoredFiles() {
 	return this->_storedContent;
 }
