cbc57e771aa105ea5d0c9a7ae9523e11ea6db944
Linsheng Ding <lding43@uwo.ca>
2018-11-27 15:27:33 -0500

    Last saved copy

--- cgi-bin/removefile.cpp
+++ cgi-bin/removefile.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,21 +10,24 @@ using namespace cgicc;
 
 int main(){
   Cgicc formdata;
-	
+/// html header for remove file	
   cout << "Content-type: text/html" << endl << endl;
   cout << "<html> \n";
   cout << "<head><title>Remove files</title></head>\n";
   cout << "<body>\n";
 
+/// grab the path using form data
   form_iterator path = formdata.getElement("path");
   
+/// translate the path into string 
   string filePath = path->getValue();
   
+/// This is the remove function that will remove the file if the filepath point to some file in the raspberry pi 
   if( remove(filePath.c_str()) != 0 ){
-    cout << "<p>file does not exist</p>";
+    cout << "<p>file does not exist</p>"; ///error message if file is not found
   }
   else
-    cout << "<p>file successfile deleted, refresh download page to see all the files on the cloud</p>";
+    cout << "<p>file successfile deleted, refresh download page to see all the files on the cloud</p>"; ///print a message if file is found and deleted 
   cout << "</body></html>";
 
   return 0;
