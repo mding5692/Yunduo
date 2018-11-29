96d14e4938fbeedcc7de5921d5cc2162dc1ecec6
Linsheng Ding <lding43@uwo.ca>
2018-11-29 12:17:28 -0500

    Updates docs

---
+++ docs/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['admin',['admin',['../classadmin.html',1,'']]]
+];
---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loginserver',['loginServer',['../classloginServer.html',1,'']]]
+];
